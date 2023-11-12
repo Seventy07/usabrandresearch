f6d78a5021ef1feea4e02023b3d83ca5089ba5c4
Seventy07 <143789418+Seventy07@users.noreply.github.com>
2023-11-12 21:10:49 +0100

    Add files via upload

---
+++ exclusive-makeup-research/continue/thanks.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="viewport" content="initial-scale=1, width=device-width, user-scalable=0" />
+	<link rel="stylesheet" type="text/css" href="works/colorful/colorful.css" />
+	<title>Product Review World - Thanks</title>
+</head>
+
+<body>
+
+	<div class="hedr">
+	     <img src="works/images/logo.png" width="119" height="59" /> 
+		 
+		 </br>
+		 </br></br></br>
+		 </br>
+		 </br>
+		<h1>Thank you for your time and for your answers too. We will contact you as soon as possible.</h1>
+	</div>
+		 
+			</div>
+			</div>		</div>
+<br><br><br><br><br>
+<div class="dwntxt">
+  <p>&copy; All rights reserved. GROCERY TASTER PROGRAM Ltd is not affiliated with any social media platform or brands. | |<a <p="" style="color: white;" href="tos.html"> Terms and Conditions </a>| |<a style="color: white;" href="privacy.html"> Privacy Policy </a> | <a style="color: white;" href="rules.html"> Participations Rules </a></p>
+</div>
+
+
+
+
+</body>
+
+</html>
